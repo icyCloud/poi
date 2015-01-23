652c4f91019b9b7c5567281e39f3cd2db14b6d1e
Sai <zhouquanbest@gmail.com>
2015-01-23 16:10:22 +0800

    update

---
+++ sample/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+Config = {
+    'db' : 'mysql://root:root@192.168.10.15:3306/devine_poi?charset=utf8',
+    'db_stock': 'mysql://btw:btw@121.40.120.72:3306/devine_stock2?charset=utf8',
+}
+
+LISTEN_IP = '0.0.0.0'
+
+API = {
+        'EBOOKING': 'http://127.0.0.1:9501',
+}
